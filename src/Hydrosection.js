3cb928aecc3ac08d6720acca3a1e46129be3d464
Amanturov7 <edi.amanturov@gmail.com>
2023-07-17 16:53:35 +0600

    1

--- src/Hydrosection.js
+++ src/Hydrosection.js
@@ -3,10 +3,13 @@ import React from 'react';
 function Hydrosection() {
   return (
     <div>
-      <h1>Гидроучасток</h1>
-      {/* Добавьте содержимое страницы */}
-    </div>
-  );
-}
+      <h1>Гидроучастки</h1>
+
+
+      <iframe src="http://10.22.22.21:5601/app/dashboards#/view/cde75150-2160-11ee-9292-4514b0406ee9?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-30d%2Cto%3Anow))&show-time-filter=true" height="900" width="1296"></iframe>
+
+
+      </div>
+      )}
 
 export default Hydrosection;
