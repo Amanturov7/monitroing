7f2c04c5f0c3de268ed918a0b430941a9aa60e1b
Amanturov7 <edi.amanturov@gmail.com>
2023-10-24 16:47:32 +0600

    sensors

--- src/KibanaDashboard.js
+++ src/KibanaDashboard.js
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import './KibanaDashboard.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import regionsData from './regionsData.json';
-
+import {
+  faBatteryFull,
+  faBatteryHalf,
+  faBatteryEmpty,
+  faBatteryThreeQuarters,
+  faBatteryQuarter,
+  faSignal,
+  faClock,
+  faToggleOn,
+  faToggleOff,
+  faTriangleExclamation,
+} from '@fortawesome/free-solid-svg-icons';
 function KibanaDashboard() {
   const [selectedRegion, setSelectedRegion] = useState(null);
   const [selectedHydro, setSelectedHydro] = useState(null);
   const [selectedSystem, setSelectedSystem] = useState(null);
   const [currentLevel, setCurrentLevel] = useState('region');
-
+  const [selectedHydropost, setSelectedHydropost] = useState(null);
   // Set "Чуйский" region as the default selection on page load
   useEffect(() => {
     setSelectedRegion('Чуйский');
@@ -18,6 +30,7 @@ function KibanaDashboard() {
     setSelectedRegion(region);
     setSelectedHydro(null);
     setSelectedSystem(null);
+    setSelectedHydropost(null);
     setCurrentLevel('hydro');
   };
 
@@ -32,6 +45,11 @@ function KibanaDashboard() {
     setCurrentLevel('dashboard');
   };
 
+  const handleHydropostClick = (hydropost) => {
+    setSelectedHydropost(hydropost);   
+    setCurrentLevel('hydropost');
+  };
+
   const handleBackClick = () => {
     if (currentLevel === 'hydropost') {
       setCurrentLevel('system');
@@ -44,6 +62,41 @@ function KibanaDashboard() {
     }
   };
 
+  function getBatteryIcon(power) {
+    if (power >= 95) {
+      return faBatteryFull;
+    } else if (power >= 90) {
+      return faBatteryThreeQuarters;
+    } else  if (power >= 60){
+      return faBatteryHalf;}
+      else if (power >= 40){
+      return faBatteryQuarter;
+      }
+      else if (power >= 10){
+        return faBatteryEmpty;
+        }
+    }
+
+
+function getStatusIcon(status) {
+  if (status === 'Активен') {
+    return faToggleOn;
+  } else if (status === 'Неактивен') {
+    return faToggleOff;
+  } else if (status === 'Вышел из строя') {
+    return faTriangleExclamation;
+  } 
+}
+
+
+    // Обновляем списки датчиков в зависимости от их статуса
+    // useState(() => {
+    //   setActiveSensors(sensorsData.filter((sensor) => sensor.status === 'Активен'));
+    //   setInactiveSensors(sensorsData.filter((sensor) => sensor.status === 'Неактивен'));
+    //   setFaultySensors(sensorsData.filter((sensor) => sensor.status === 'Вышел из строя'));
+    // }, []);
+  
+  
   // const [arraySensors, setArraySensors] = useState([])
 
   // const fetchedSensors = fetch(`http//localhost:8080/api/dashboard/sensors`).then((response =>  setArraySensors(response)))
@@ -122,11 +175,57 @@ function KibanaDashboard() {
                 <div className="dashboard-columns">
                   {selectedSystem.hydroposts.map((hydropost, index) => (
                     <div key={index} className="dashboard-block">
-                      <div className="buttonHydropost">{hydropost.title}</div>
+                          <div>
+                        <button onClick={() => handleHydropostClick(hydropost)} className="hydro-button">
+                          {hydropost.title}
+                        </button>
+                      </div>
+
+                      {/* <div className="buttonHydropost">{hydropost.title}</div> */}
                       <iframe title={hydropost.title} src={hydropost.url} height="400" width="830" />
                     </div>
                   ))}
                 </div>
+              </div>
+            )}
+
+            {currentLevel === 'hydropost' && (
+              <div>
+              <button onClick={handleBackClick} className="hydro-button">
+                <h2>{selectedHydropost.title}</h2>
+              </button>
+              <div className="dashboard-columns">
+                {selectedHydropost.sensor.map((sensor, index) => (
+                  <div key={index} className="sensor-block1">
+                    <div className="buttonHydropost"> Датчик {sensor.sensor_name}</div>
+                          {/* <div className="sensor-block1"> */}
+                            {/* <div className="sensor-info"> */}
+                              <h6 className="sensor-item-title">{sensor.sensor_name}</h6>
+                              <p className="sensor-item-status">
+                                <FontAwesomeIcon icon={getStatusIcon(sensor.status)} style={{ marginRight: '10px' }}/>
+                                {sensor.status}
+                              </p>
+                              <p className="sensor-item-last-updated">
+                                <FontAwesomeIcon icon={faClock} style={{ marginRight: '10px' }}/>
+                                Последнее обновление: {sensor.last_transmission}
+                              </p>
+                              <p className="sensor-item-battery">
+                              <FontAwesomeIcon icon={getBatteryIcon(sensor.power)}style={{ marginRight: '10px' }} />                      
+                              Заряд: {sensor.power}%
+                              </p>
+                              <p className="sensor-item-signal">
+                                <FontAwesomeIcon icon={faSignal} style={{ marginRight: '10px' }} />
+                                Уровень сигнала: {sensor.signal}%
+                              </p>
+                              <p className="sensor-item-signal">
+                              {}
+                              </p>
+                              
+                            {/* </div>
+                          </div> */}
+                  </div>
+                ))}
+              </div>
               </div>
             )}
           </div>
